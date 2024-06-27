28bc7c40db4b2c85bab1ccdab44f43c6515b0a05
Endi Hıdır <32939545+endihidir@users.noreply.github.com>
2024-06-27 11:57:24 +0300

    unused classes removed from main lifetime scope

--- Assets/UnityBase/Scripts/LifeTimeScopes/AppLifetimeScope.cs
+++ Assets/UnityBase/Scripts/LifeTimeScopes/AppLifetimeScope.cs
@@ -46,20 +46,15 @@ namespace UnityBase.BaseLifetimeScope
             builder.Register<TutorialActionManager>(Lifetime.Singleton).AsImplementedInterfaces();
             builder.Register<TutorialMaskManager>(Lifetime.Singleton).AsImplementedInterfaces();
             builder.Register<TutorialProcessManager>(Lifetime.Singleton).AsImplementedInterfaces();
-
-            builder.Register<CommandManager>(Lifetime.Singleton).AsImplementedInterfaces();
-            builder.Register<CoinManager>(Lifetime.Singleton).AsImplementedInterfaces();
-            builder.Register<TaskManager>(Lifetime.Singleton).AsImplementedInterfaces();
-            builder.Register<SwipeManager>(Lifetime.Singleton).AsImplementedInterfaces();
             
             builder.Register<ViewBehaviourFactory>(Lifetime.Singleton).AsImplementedInterfaces();
             builder.Register<ButtonBehaviourFactory>(Lifetime.Singleton).AsImplementedInterfaces();
+            builder.Register<JsonDataManager>(Lifetime.Singleton).AsImplementedInterfaces();
         }
 
         private void RegisterScopedServices(IContainerBuilder builder)
         {
-            builder.Register<JsonDataManager>(Lifetime.Scoped).AsImplementedInterfaces();
-            builder.Register<CommandRecorder>(Lifetime.Scoped).AsImplementedInterfaces();
+         
         }
         
         private void RegisterTransientServices(IContainerBuilder builder)
